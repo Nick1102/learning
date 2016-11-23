fd33a4b2e1a712afa370eb5279469edb398cb5a9
mxchan <mingxiangchan@gmail.com>
2016-11-23 18:04:33 +0800

    fully working virtualbox setup script

--- setup/virtualbox.sh
+++ setup/virtualbox.sh
@@ -1,9 +1,23 @@
+cd
+echo "What is the name of the VM to be created(default: base-ubuntu)"
+read VM_NAME
+if [[ -z "$VM_NAME" ]]; then VM_NAME="base-ubuntu"; fi
 
-#change these variables as needed
-VM_NAME="base-ubuntu"
-UBUNTU_ISO_PATH=~/Documents/ubuntu-16.04.1-server-amd64.iso
-VM_HD_PATH=~/Documents/base-ubuntu.vdi # The path to VM hard disk (to be created).
-SHARED_PATH=~ # Share home directory with the VM
+echo "What is the path to the ISO image for the VM(default: ~/Downloads/ubuntu-16.04.1-server-amd64.iso)"
+read UBUNTU_ISO_PATH
+if [[ -z "$UBUNTU_ISO_PATH" ]]; then UBUNTU_ISO_PATH=~/Downloads/ubuntu-16.04.1-server-amd64.iso ; fi
+
+echo "What is the path to the HDD image to be created(default: ~/Documents/base-ubuntu.vdi)"
+read VM_HD_PATH
+if [[ -z "$VM_HD_PATH" ]]; then VM_HD_PATH=~/Documents/base-ubuntu.vdi; fi
+
+echo "What is the path to the folder to be shared with the VM?(default: ~)"
+read SHARED_PATH
+if [[ -z "$SHARED_PATH" ]]; then SHARED_PATH=~; fi
+
+echo "Do you want to link the SSH port in the VM(22) to a port on localhost?(default: 2222)"
+read SSH_PORT
+if [[ -z "$SSH_PORT" ]]; then SSH_PORT=2222; fi
 
 vboxmanage createvm --name $VM_NAME --ostype Ubuntu_64 --register
 vboxmanage createhd --filename $VM_HD_PATH --size 10768
@@ -14,6 +28,6 @@ vboxmanage storageattach $VM_NAME --storagectl "IDE Controller" --port 0 --devic
 vboxmanage modifyvm $VM_NAME --ioapic on
 vboxmanage modifyvm $VM_NAME --memory 1024 --vram 128
 vboxmanage modifyvm $VM_NAME --nic1 nat
-vboxmanage modifyvm $VM_NAME --natpf1 "guestssh,tcp,,2222,,22"
+vboxmanage modifyvm $VM_NAME --natpf1 "guestssh,tcp,,$SSH_PORT,,22"
 vboxmanage modifyvm $VM_NAME --natdnshostresolver1 on
 vboxmanage sharedfolder add $VM_NAME --name shared --hostpath $SHARED_PATH --automount
