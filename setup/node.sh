14219500f34e5c1b9981e82fdb77fc9601278cd2
Ming Xiang <mingxiang@nextacademy.com>
2016-11-25 15:24:20 +0800

    split node and rails setup

---
+++ setup/node.sh
@@ -0,0 +1,6 @@
+
+$( curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash)
+export NVM_DIR="/home/engineering/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install node
+nvm use node
--- setup/rails.sh
+++ setup/rails.sh
@@ -1,7 +1,3 @@
-curl -sL https://deb.nodesource.com/setup_4.x
-source ~/.bashrc
-nvm install node
-nvm use node
 
 gem install rails -v 4.2.6
 rbenv rehash
