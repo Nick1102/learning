ca7b75ea569fad1c19c5f966d2df6a30b42bb168
mxchan <mingxiangchan@gmail.com>
2016-12-07 22:00:48 +0800

    fixed node script

--- setup/node.sh
+++ setup/node.sh
@@ -1,6 +1,6 @@
 
 $( curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash)
-export NVM_DIR="/home/engineering/.nvm"
+export NVM_DIR="/home/$USER/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install node
 nvm use node
--- vim/nvim_setup.sh
+++ vim/nvim_setup.sh
@@ -6,6 +6,7 @@ copy_files()
 
 symlink_nvim()
 {
+    mkdir ~/.vim
     mkdir ~/.config
     ln -s ~/.vimrc ~/.config/nvim/init.vim
     ln -s ~/.vim ~/.config/nvim
