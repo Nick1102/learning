0cd9d4ac283c8decc9a2e726f397ed20de38b9ae
mxchan <mingxiangchan@gmail.com>
2016-12-30 15:31:32 +0800

    updated postgresql scripts

---
+++ setup/postgresql.sh
@@ -0,0 +1,5 @@
+sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y postgresql-common
+sudo apt-get install -y postgresql-9.5 libpq-dev
--- setup/rails.sh
+++ setup/rails.sh
@@ -1,12 +1,3 @@
 
 gem install rails -v 4.2.6
 rbenv rehash
-
-sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
-wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y postgresql-common
-sudo apt-get install -y postgresql-9.5 libpq-dev
-
-u = "$USER"
-sudo -u postgres createuser $u -s
